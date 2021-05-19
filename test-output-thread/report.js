65637d74ef7b8fd48516cbf6d8b965e678f4642a
Dileep <dileepkumarpaidi@gmail.com>
2021-05-19 15:53:43 +0530

    BDD cucumber Junit parallel execution

---
+++ test-output-thread/report.js
@@ -0,0 +1,95 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "ea1708f6-a1ec-4f72-9e75-5a0c285564a3",
+    "feature": "Duplicate Account Page Feature",
+    "scenario": "Duplicate Accounts page title",
+    "start": 1621419134202,
+    "group": 14,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1621419157260,
+    "className": "failed"
+  },
+  {
+    "id": "4f254080-efa0-4e94-8861-81822097406f",
+    "feature": "Login page feature",
+    "scenario": "Login page title",
+    "start": 1621419134199,
+    "group": 16,
+    "content": "",
+    "tags": "",
+    "end": 1621419156619,
+    "className": "failed"
+  },
+  {
+    "id": "7fba7f1b-2180-4418-b6a7-cc3ab1e1c028",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts page title",
+    "start": 1621419134204,
+    "group": 15,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1621419174158,
+    "className": "failed"
+  },
+  {
+    "id": "2a637625-fe7c-4f7b-b34a-fdb18a3921a7",
+    "feature": "Duplicate Account Page Feature",
+    "scenario": "Duplicate Accounts section count",
+    "start": 1621419157284,
+    "group": 14,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1621419173133,
+    "className": "failed"
+  },
+  {
+    "id": "d547163e-6bf9-4b57-b638-eac6d4c48b20",
+    "feature": "Login page feature",
+    "scenario": "Login with correct credentials",
+    "start": 1621419171792,
+    "group": 16,
+    "content": "",
+    "tags": "",
+    "end": 1621419182773,
+    "className": "failed"
+  },
+  {
+    "id": "4820ee77-50c1-424b-ad71-3c9be5a25afc",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts section count",
+    "start": 1621419174167,
+    "group": 15,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1621419185469,
+    "className": "failed"
+  },
+  {
+    "id": "cd54455d-7f14-43eb-a6db-56ad7b527c5c",
+    "feature": "Login page feature",
+    "scenario": "Forgot Password link",
+    "start": 1621419156647,
+    "group": 16,
+    "content": "",
+    "tags": "",
+    "end": 1621419171780,
+    "className": "failed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 14,
+    "content": "Thread[pool-1-thread-1,5,main]"
+  },
+  {
+    "id": 15,
+    "content": "Thread[pool-1-thread-2,5,main]"
+  },
+  {
+    "id": 16,
+    "content": "Thread[pool-1-thread-3,5,main]"
+  }
+]);
+});
